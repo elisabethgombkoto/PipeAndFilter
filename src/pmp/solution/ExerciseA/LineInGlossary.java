0cc5720097646bc10ec7bc74adb40bc8a1acd6f7
elisabethgombkoto <ego8769@students.fhv.at>
2017-10-27 15:24:13 +0200

    sysout is not working in Run what ever goes wrong no idea what

---
+++ src/pmp/solution/ExerciseA/LineInGlossary.java
@@ -0,0 +1,23 @@
+package pmp.solution.ExerciseA;
+
+/**
+ * Created by Elisabeth on 27.10.2017.
+ */
+public class LineInGlossary {
+
+  private int _i;
+  private String _s;
+
+  public LineInGlossary(int i, String s) {
+    _i=i;
+    _s=s;
+  }
+
+  public int get_index() {
+    return _i;
+  }
+
+  public String get_string() {
+    return _s;
+  }
+}
