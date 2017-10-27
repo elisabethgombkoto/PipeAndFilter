10e15e825df8f2124d748524a540dc5f78c04259
elisabethgombkoto <ego8769@students.fhv.at>
2017-10-27 16:33:30 +0200

    i just disturbed everything

--- src/pmp/solution/ExerciseA/Input.java
+++ src/pmp/solution/ExerciseA/Input.java
@@ -1,14 +1,18 @@
 package pmp.solution.ExerciseA;
 
+import pmp.filter.Source;
+import pmp.interfaces.Readable;
+
 import java.io.*;
 import java.lang.reflect.Array;
+import java.nio.CharBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Elisabeth on 27.10.2017.
  */
-public class Input {
+public class Input extends Source<String> implements Readable<String>, Runnable{
 
   private String _path;
 
@@ -23,14 +27,12 @@ public class Input {
     int lineindex=0;
 
     try (BufferedReader br = new BufferedReader(new FileReader(new File(_path)))) {
-      String s;
-      while ((s = br.readLine()) != null) {
+
+      while (this.read() != null) {
+        String s = this.read();
         LineInGlossary line = new LineInGlossary(lineindex+1, s);
         lines.add(lineindex, line);
         lineindex++;
-
-        System.out.print(line.get_index());
-       System.out.println(line.get_string());
       }
     } catch (FileNotFoundException e1) {
       e1.printStackTrace();
@@ -39,4 +41,22 @@ public class Input {
     }
     return lines;
   }
+
+  @Override
+  public String read() throws StreamCorruptedException {
+    String s = null ;
+    try (BufferedReader br = new BufferedReader(new FileReader(new File(_path)))) {
+
+      while ((s = br.readLine()) != null) {
+        s = br.readLine();
+
+      }
+    } catch (FileNotFoundException e1) {
+      e1.printStackTrace();
+    } catch (IOException e1) {
+      e1.printStackTrace();
+    }
+    return s;
+  }
+
 }
