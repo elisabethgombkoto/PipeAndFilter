8556e63a50486629815e28575be8a70e7289235d
elisabethgombkoto <ego8769@students.fhv.at>
2017-10-30 00:40:10 +0100

    LineConstructor is  reade, but have to be tested

--- src/pmp/solution/ExerciseB/LineConstructorFilter.java
+++ src/pmp/solution/ExerciseB/LineConstructorFilter.java
@@ -5,6 +5,7 @@ import pmp.interfaces.Readable;
 import pmp.solution.ExerciseA.LineFromSequenceOfWords;
 
 import java.security.InvalidParameterException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,6 +19,22 @@ public class LineConstructorFilter extends DataTransformationFilter2<List<String
 
   @Override
   protected List<LineFromSequenceOfWords> process(List<String> entity) {
-    return null;
+
+    List<LineFromSequenceOfWords> lines = new ArrayList<>();
+    int counter = 0;
+    int lineIndex= 1;
+    String[] str = null;
+    for(int i = 0; i<entity.size(); i++){
+      str[counter] = entity.get(i);
+      if ((counter % 5)==0) {
+        LineFromSequenceOfWords line = new LineFromSequenceOfWords(lineIndex, str);
+        lines.add(line);
+        counter = 0;
+        lineIndex++;
+        str = null;
+      }
+      counter++;
+    }
+    return lines;
   }
 }
