4685a4860e163bd5f590c854907a82b1975cbbf5
elisabethgombkoto <ego8769@students.fhv.at>
2017-10-30 00:23:01 +0100

    findWords is ready

--- src/pmp/solution/ExerciseB/WordConstructorFilter.java
+++ src/pmp/solution/ExerciseB/WordConstructorFilter.java
@@ -20,32 +20,29 @@ public class WordConstructorFilter extends DataTransformationFilter2<String,List
 
   @Override
   protected List<String> process(String entity) {
-
-  ArrayList<String> words = new ArrayList<>();
-  StringBuilder st = null;
+    ArrayList<String> words = new ArrayList<>();
+    StringBuilder st = null;
     for(int i = 0; i<entity.toCharArray().length; i++){
-    char c = entity.toCharArray()[i];
-    if(Character.isLetter(c)){  //is a letter
-      if(st!=null){
-        st.append(c);// if it is not null then include a letter
-      }else {
-        st = new StringBuilder().append(c);
-      }
-    }else { // is not a letter
-      if(st!=null){ // but st include alredy some letter
-        words.add(st.toString()); //put to array st in array
-        st = null;
+      char c = entity.toCharArray()[i];
+      if(Character.isLetter(c)){  //is a letter
+        if(st!=null){
+          st.append(c);// if it is not null then include a letter
+        }else {
+          st = new StringBuilder().append(c);
+        }
+      }else { // is not a letter
+        if(st!=null){ // but st include alredy some letter
+          words.add(st.toString()); //put to array st in array
+          st = null;
+        }
       }
     }
-  }
     if(st!=null){
-    //put to array st whit the letters
-    // if the last char is not a later then st is null
-    // and we do not have to empty the st
-    words.add(st.toString());
+      //put to array st whit the letters
+      // if the last char is not a later then st is null
+      // and we do not have to empty the st
+      words.add(st.toString());
+    }
+    return words;
   }
-
-
-   return words;
-}
 }
